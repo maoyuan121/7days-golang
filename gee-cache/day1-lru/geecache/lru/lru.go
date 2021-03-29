9e63e6a253e5dbfb801c0d9b4340e96e1d6b85c0
毛远鸣 <yuanming.mao@rightic.cn>
2021-03-29 17:11:59 +0800

    读代码 gee-cache day1

--- gee-cache/day1-lru/geecache/lru/lru.go
+++ gee-cache/day1-lru/geecache/lru/lru.go
@@ -2,13 +2,14 @@ package lru
 
 import "container/list"
 
-// Cache is a LRU cache. It is not safe for concurrent access.
+// LRU = Least Recently Used 最近最少使用。 It is not safe for concurrent access.
 type Cache struct {
-	maxBytes int64
-	nbytes   int64
+	maxBytes int64 // 允许缓存的最大字节数
+	nbytes   int64 // 所有缓存数据的字节数
 	ll       *list.List
 	cache    map[string]*list.Element
-	// optional and executed when an entry is purged.
+
+	// 可选的，在条目被清除时执行。
 	OnEvicted func(key string, value Value)
 }
 
@@ -18,11 +19,12 @@ type entry struct {
 }
 
 // Value use Len to count how many bytes it takes
+// Value 定义了 Len() 函数，其用于计算他占用了多少个字节
 type Value interface {
 	Len() int
 }
 
-// New is the Constructor of Cache
+// New 是 Cache  的构造函数
 func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
 	return &Cache{
 		maxBytes:  maxBytes,
@@ -32,7 +34,9 @@ func New(maxBytes int64, onEvicted func(string, Value)) *Cache {
 	}
 }
 
-// Add adds a value to the cache.
+// 添加一个值到缓存
+// 如果已经存在，将该元素移动到最前面，否则添加改元素到第一个位置。
+// 如果缓存的内容的大小（nbytes）超过了允许的最大字节数（maxBytes），那么删除最老的一个缓存
 func (c *Cache) Add(key string, value Value) {
 	if ele, ok := c.cache[key]; ok {
 		c.ll.MoveToFront(ele)
@@ -49,7 +53,8 @@ func (c *Cache) Add(key string, value Value) {
 	}
 }
 
-// Get look ups a key's value
+// 根据 key 获取值
+// 如果获取到了，该元素移动到链表的最前面
 func (c *Cache) Get(key string) (value Value, ok bool) {
 	if ele, ok := c.cache[key]; ok {
 		c.ll.MoveToFront(ele)
@@ -59,7 +64,9 @@ func (c *Cache) Get(key string) (value Value, ok bool) {
 	return
 }
 
-// RemoveOldest removes the oldest item
+// 移除最老的  cache
+// 重新计算缓存总字节数
+// 如果定义了 OnEvicted，那么触发它
 func (c *Cache) RemoveOldest() {
 	ele := c.ll.Back()
 	if ele != nil {
@@ -73,7 +80,7 @@ func (c *Cache) RemoveOldest() {
 	}
 }
 
-// Len the number of cache entries
+// 返回缓存中元素的的个数
 func (c *Cache) Len() int {
 	return c.ll.Len()
 }
